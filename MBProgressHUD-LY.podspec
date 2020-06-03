28e9b9098aa833c6baa1785c84228ffc486fd8fa
ButtFly <315585758@qq.com>
2020-06-03 10:32:36 +0800

    0.2.0

--- MBProgressHUD-LY.podspec
+++ MBProgressHUD-LY.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
     s.name             = 'MBProgressHUD-LY'
-    s.version          = '0.1.0'
+    s.version          = '0.2.0'
     s.summary          = 'MBProgressHUD的封装，自用'
     
     # This description is used to generate tags and improve search results.
@@ -40,5 +40,5 @@ Pod::Spec.new do |s|
     
     # s.public_header_files = 'Pod/Classes/**/*.h'
     # s.frameworks = 'UIKit', 'MapKit'
-    s.dependency 'MBProgressHUD', '~> 1.2.0'
+    s.dependency 'MBProgressHUD'
 end
--- MBProgressHUD-LY/Classes/MBProgressHUD-LY.swift
+++ MBProgressHUD-LY/Classes/MBProgressHUD-LY.swift
@@ -27,78 +27,84 @@ extension UIImage {
 
 open class LYProgressHUD: MBProgressHUD {
     
+    //HUD的背景颜色
+    static var backgroudColor:UIColor = UIColor.black.withAlphaComponent(0.9)
+    //HUD的内容颜色
+    static var textColor:UIColor = .white
+    ///当HUD出现时，当前控制器是否停止继续响应
+    static var isStopUserInteractionEnabledWhenAppear:Bool = true
+    
     open func show(animated: Bool, autoHideDelay: TimeInterval) {
         show(animated: animated)
+        self.isUserInteractionEnabled = LYProgressHUD.isStopUserInteractionEnabledWhenAppear
+        backgroundColor = .clear
+        bezelView.backgroundColor = LYProgressHUD.backgroudColor
+        contentColor = LYProgressHUD.textColor
+        
+        removeFromSuperViewOnHide = true
+        label.numberOfLines = 0
+        
         if autoHideDelay > 0 {
             hide(animated: animated, afterDelay: autoHideDelay)
         }
     }
     
     open func loadingHud(text: String?) -> Void {
-        
         mode = .indeterminate
-        removeFromSuperViewOnHide = true
-        
         customView = nil
-
-        backgroundColor = .clear
-        bezelView.backgroundColor = UIColor.black.withAlphaComponent(0.9)
-        contentColor = .white
         label.text = text
-        label.numberOfLines = 0
         
     }
     
     open func textHud(text: String?) -> Void {
         
         mode = .text
-        removeFromSuperViewOnHide = true
-        
         customView = nil
-        
-        backgroundColor = .clear
-        bezelView.backgroundColor = UIColor.black.withAlphaComponent(0.9)
-        contentColor = .white
         label.text = text
-        label.numberOfLines = 0
         
     }
     
     open func successHud(text: String?) -> Void {
 
         mode = .customView
-        removeFromSuperViewOnHide = true
-
         customView = nil
-        
-        backgroundColor = .clear
-        bezelView.backgroundColor = UIColor.black.withAlphaComponent(0.9)
         let iv = UIImageView(frame: CGRect(x: 0, y: 0, width: 37, height: 37))
         iv.image = UIImage.ly_inSelfBundleNamed("hud_success_bgi")
         customView = iv
-        contentColor = .white
         label.text = text
-        label.numberOfLines = 0
         
     }
     
     open func failureHud(text: String?) -> Void {
 
         mode = .customView
-        removeFromSuperViewOnHide = true
-        
         customView = nil
-        
-        backgroundColor = .clear
-        bezelView.backgroundColor = UIColor.black.withAlphaComponent(0.9)
         let iv = UIImageView(frame: CGRect(x: 0, y: 0, width: 37, height: 37))
         iv.image = UIImage.ly_inSelfBundleNamed("hud_failure_bgi")
         customView = iv
-        contentColor = .white
         label.text = text
-        label.numberOfLines = 0
-
+        
     }
+    
+    ///style:   1-横着的条形进度条。2-圆环形进度圈，进度在里圈。3-菊花转。其他：圆形进度条，已进行的部分是实体色，未进行的事浅色
+    open func progressHud(proress:CGFloat,text:String?,style:Int=1) {
+        
+        switch style {
+        case 1:
+            mode = .determinateHorizontalBar
+        case 2:
+            mode = .determinate
+        case 3:
+            mode = .indeterminate
+        default:
+            mode = .annularDeterminate
+        }
+        
+        label.text = text
+        progress = Float(proress)
+        
+    }
+    
     
 }
 
@@ -139,42 +145,61 @@ public extension UIViewController {
         }
     }
     
-    func ly_showLoadingHUD(text: String?, autoHideDelay: TimeInterval = 0) -> Void {
+    func ly_showLoadingHUD(text: String?, autoHideDelay: TimeInterval = 0, inView: UIView? = nil) -> Void {
         DispatchQueue.ly_mbph_runInMain {
+            let view: UIView = inView ?? self.view
             self.lyHud.loadingHud(text: text)
-            self.view.addSubview(self.lyHud)
+            view.addSubview(self.lyHud)
             self.lyHud.show(animated: true, autoHideDelay: autoHideDelay)
         }
     }
     
-    func ly_showTextHub(text: String?, autoHideDelay: TimeInterval = 0) -> Void {
+    func ly_showTextHub(text: String?, autoHideDelay: TimeInterval = 0, inView: UIView? = nil) -> Void {
         DispatchQueue.ly_mbph_runInMain {
+            let view: UIView = inView ?? self.view
             self.lyHud.textHud(text: text)
-            self.view.addSubview(self.lyHud)
+            view.addSubview(self.lyHud)
+            self.lyHud.progress = 0.5
             self.lyHud.show(animated: true, autoHideDelay: autoHideDelay)
         }
     }
     
-    func ly_showSuccessHud(text: String?, autoHideDelay: TimeInterval = 0) -> Void {
+    func ly_showSuccessHud(text: String?, autoHideDelay: TimeInterval = 0, inView: UIView? = nil) -> Void {
         DispatchQueue.ly_mbph_runInMain {
+            let view: UIView = inView ?? self.view
             self.lyHud.successHud(text: text)
-            self.view.addSubview(self.lyHud)
+            view.addSubview(self.lyHud)
+            self.lyHud.isUserInteractionEnabled = false
             self.lyHud.show(animated: true, autoHideDelay: autoHideDelay)
         }
     }
     
-    func ly_showFailureHud(text: String?, autoHideDelay: TimeInterval = 0) -> Void {
+    func ly_showFailureHud(text: String?, autoHideDelay: TimeInterval = 0, inView: UIView? = nil) -> Void {
         DispatchQueue.ly_mbph_runInMain {
+            let view: UIView = inView ?? self.view
             self.lyHud.failureHud(text: text)
-            self.view.addSubview(self.lyHud)
+            view.addSubview(self.lyHud)
             self.lyHud.show(animated: true, autoHideDelay: autoHideDelay)
         }
     }
     
-    func ly_hideHub(afterDelay delay: TimeInterval = 0) -> Void {
+    func ly_hideHud(afterDelay delay: TimeInterval = 0) -> Void {
         DispatchQueue.ly_mbph_runInMain {
             self.lyHud.hide(animated: true, afterDelay: delay)
         }
     }
     
+    func ly_showProgressHud(progress:CGFloat, text:String?, style:Int = 1, autoHideDelay: TimeInterval = 0, inView: UIView? = nil)  {
+        
+        DispatchQueue.ly_mbph_runInMain {
+            let view: UIView = inView ?? self.view
+            self.lyHud.progressHud(proress: progress, text: text, style: style)
+            view.addSubview(self.lyHud)
+            self.lyHud.show(animated: true, autoHideDelay: autoHideDelay)
+        }
+    }
+    
+    
 }
+
+
